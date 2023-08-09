cecbed6b3ea01c5ed1dbb1bd0fb6046ec0ee8041
Aleksandr <102950184+Scheegl@users.noreply.github.com>
2023-08-09 16:29:26 -0300

    Add models: SiteUser Lots Reply News

--- pwmarket/market/models.py
+++ pwmarket/market/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.cache import cache
 
-# Create your models here.
+
+class SiteUser(models.Model):
+    site_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating_user = models.IntegerField(default=0)
+
+    def update_rating(self):
+        pass
+
+
+class Lots(models.Model):
+    lots_user = models.ForeignKey("SiteUser", on_delete=models.CASCADE)
+
+    CHARACTER = 'CR'
+    MONEY = 'MN'
+    THINGS = 'TN'
+    SERVICES = 'SV'
+
+    CATEGORY_CHOICES = (
+        (CHARACTER, 'Персонаж'),
+        (MONEY, 'Юани'),
+        (THINGS, 'Предметы'),
+        (SERVICES, 'Услуги')
+    )
+    category_choice = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
+    time_creation = models.DateTimeField(auto_created=True)
+    title = models.CharField(max_length=24)
+    description = models.TextField()
+
+    # screenshot =
+
+    def get_absolute_url(self):
+        return f'/lots/{self.id}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        cache.delete(f'lots-{self.pk}')
+
+
+class Reply(models.Model):
+    reply_lots = models.ForeignKey('Lots', on_delete=models.CASCADE)
+    reply_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=256)
+    time_creation = models.DateTimeField(auto_now_add=True)
+
+
+
+class News(models.Model):
+    pass
