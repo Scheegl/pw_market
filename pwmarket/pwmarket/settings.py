7f312875fa7c5653727546e6dcd0d447480ceb4a
Aleksandr <102950184+Scheegl@users.noreply.github.com>
2023-08-25 15:55:55 -0300

    Add models: login etc

--- pwmarket/pwmarket/settings.py
+++ pwmarket/pwmarket/settings.py
@@ -160,6 +160,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = "crypto.sch@yandex.ru"
-EMAIL_HOST_PASSWORD = "gtigvhsocmwwvmqx"
+EMAIL_HOST_PASSWORD = "xxx"
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
